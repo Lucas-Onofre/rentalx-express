aa9d777b121950110601a7dacfbc6b6ecc72876f
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2022-04-28 13:20:01 -0300

    Altering User Table

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -12,15 +12,13 @@ class UsersRepository implements IUsersRepository {
   }
 
   async create({
-    name,
-    username,
+    name
     email,
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
       email,
       driver_license,
       password,
